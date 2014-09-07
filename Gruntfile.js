6892cdc2d00b0ade544f5e96d01c6be86629cd46
Christopher Hiller <chiller@badwing.com>
2014-09-06 22:38:23 -0700

    didn't work. let's try it w/ travis.  again.

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,19 +81,13 @@ module.exports = function (grunt) {
 
     clean: {
       readme: 'guts/ketch.md'
-    },
-
-    codeclimate: {
-      options: {
-        file: 'coverage/lcov.info',
-        token: '28d125c7f65a97007ae1d42cd0de10d04d23613f439640180b2087a89c8cc67c'
-      }
     }
+
   });
 
   require('load-grunt-tasks')(grunt);
 
-  grunt.registerTask('test', ['jshint', 'mochacov:main', 'mochacov:coveralls', 'codeclimate']);
+  grunt.registerTask('test', ['jshint', 'mochacov:main', 'mochacov:coveralls']);
   grunt.registerTask('html-cov', ['mochacov:html-cov']);
 
   grunt.registerTask('docs', ['jsdox', 'copy', 'clean']);
