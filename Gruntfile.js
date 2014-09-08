137a7e3f632ab4550b015132913bac8128b31fd5
Christopher Hiller <chiller@badwing.com>
2014-09-08 09:01:35 -0700

    more screwing w/ coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,21 +81,12 @@ module.exports = function (grunt) {
 
     clean: {
       readme: 'guts/ketch.md'
-    },
-
-    codeclimate: {
-      options: {
-        file: 'coverage/lcov.info',
-        token: '28d125c7f65a97007ae1d42cd0de10d04d23613f439640180b2087a89c8cc67c'
-      }
     }
-
   });
 
   require('load-grunt-tasks')(grunt);
 
-  grunt.registerTask('test', ['jshint', 'mochacov:main']);
-  grunt.registerTask('coverage', ['mochacov:lcov', 'codeclimate']);
+  grunt.registerTask('test', ['jshint', 'mochacov:main', 'mochacov:lcov']);
   grunt.registerTask('html-cov', ['mochacov:html-cov']);
 
   grunt.registerTask('docs', ['jsdox', 'copy', 'clean']);
